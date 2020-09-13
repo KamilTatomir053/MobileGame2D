db965f6e41968485faefcfc4dbffa622fd5c0213
KamilTatomir053 <kamiltatomir053@gmail.com>
2020-09-13 22:33:49 +0200

    game

---
+++ Assets/boxes.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boxes : MonoBehaviour
+{
+    public Transform[] boxPositions;
+
+    public GameObject box;
+
+    public float spawnPercentage = 0.3f;
+
+    public float repeatTime = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Boxes", repeatTime, repeatTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Boxes()
+    {
+        for (int i = 0; i < boxPositions.Length; i++)
+        {
+            float spawnRandom = Random.Range(0f, 1f);
+
+            if (spawnRandom < spawnPercentage)
+            {
+                GameObject boxObject = Instantiate(box);
+
+                boxObject.transform.position = boxPositions[i].position;
+
+                boxObject.SetActive(true);
+            }
+        }
+    }
+
+    public void Stop()
+    {
+        CancelInvoke("Boxes");
+    }
+}
---
+++ Assets/fox.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class fox : MonoBehaviour
+{
+    public Transform[] positions;
+
+    public int position = 2;
+
+    public float speed = 0.1f;
+
+    int points = 0;
+
+    public Text score;
+
+    public GameObject button;
+
+    public GameObject boxes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, positions[position].position, speed);
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) Left();
+        if (Input.GetKeyDown(KeyCode.RightArrow)) Right();
+    }
+
+    public void Left()
+    {
+        if (position > 0)
+        {
+            position--;
+        }
+    }
+
+    public void Right()
+    {
+        if (position < positions.Length - 1)
+        {
+            position++;
+        }
+    }
+
+    public void AddPoint()
+    {
+        points++;
+
+        score.text = "Score: " + points;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("box") == true)
+        {
+            button.SetActive(true);
+
+            gameObject.SetActive(false);
+
+            boxes.GetComponent<boxes>().Stop();
+        }
+    }
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
